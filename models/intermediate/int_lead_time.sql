456f60997ec40864e7114f73e6146974d1ce6f9b
Luiza Perin <lurperin@gmail.com>
2024-06-04 13:52:41 -0300

    add intermediate model and schema

---
+++ models/intermediate/int_lead_time.sql
@@ -0,0 +1,16 @@
+WITH
+
+source_data AS (
+    SELECT
+        DATE_DIFF(receving_date, PO_date, DAY) AS lead_time
+        , DATE_DIFF (invoice_date, receving_date, DAY) AS invoice_process_time
+        , DATE_DIFF(pay_date, invoice_date, DAY) AS payment_time
+        , DATE_DIFF(pay_date ,PO_date, DAY) AS total_process_time
+    FROM
+        {{ ref('stg_purchases') }}
+)
+
+SELECT
+    *
+FROM
+    source_data
