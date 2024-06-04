50c9a80f5089c99d0b3d6d5c3c073c7194a35123
Luiza Perin <lurperin@gmail.com>
2024-06-04 14:16:35 -0300

    add columns brand and description to int_lead_time model

--- models/intermediate/int_lead_time.sql
+++ models/intermediate/int_lead_time.sql
@@ -2,7 +2,9 @@ WITH
 
 source_data AS (
     SELECT
-        DATE_DIFF(receving_date, PO_date, DAY) AS lead_time
+        brand
+        , description
+        , DATE_DIFF(receving_date, PO_date, DAY) AS lead_time
         , DATE_DIFF (invoice_date, receving_date, DAY) AS invoice_process_time
         , DATE_DIFF(pay_date, invoice_date, DAY) AS payment_time
         , DATE_DIFF(pay_date ,PO_date, DAY) AS total_process_time
