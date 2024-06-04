fb605571e5ac50e16494b34c7d4a809a172fbe32
Luiza Perin <lurperin@gmail.com>
2024-06-04 14:42:22 -0300

    added brand and description columns to fct_lead_time table

--- models/fact/fct_lead_time.sql
+++ models/fact/fct_lead_time.sql
@@ -2,11 +2,15 @@ WITH
 
 source_data AS (
     SELECT
-        MAX(lead_time) AS max_lead_time
+        brand,
+        description
+        , MAX(lead_time) AS max_lead_time
         , MIN(lead_time) AS min_lead_time
         , AVG(lead_time) AS avg_lead_time
     FROM
         {{ ref('int_lead_time') }}
+    GROUP BY
+        brand, description
 )
 
 SELECT
